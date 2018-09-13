703c4a1c3e5d366945ec79a3f1a6e59545c8ca36
aOpenbrier <aopenbrier@gmail.com>
2018-09-13 16:24:32 -0700

    pseudocode outline

---
+++ assets/js/app.js
@@ -0,0 +1,13 @@
+//Update schedule function
+    //create row for each train object in array
+    //Display train info in columns
+        //get name, destination, first arrival and frequency from firebase
+            //format arrival time
+        //calculate minutes until arrival
+
+
+//Add button function
+    //prevent page reload
+    //create object in database with input fields
+        //format input for first arrival and frequency time
+    //update schedule
